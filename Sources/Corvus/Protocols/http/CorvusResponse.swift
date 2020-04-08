f87f94870d6d2eff7656a2c7a998ab92f2085e18
jhoogstraat <34964599+jhoogstraat@users.noreply.github.com>
2020-04-08 09:38:55 +0200

    Feature/response modifier (#10)
    * Implementation ResponseModifier
    
    * Adds documentation
    
    * Adds ResponseModifier test
    
    * Fixes Swiftlint errors

---
+++ Sources/Corvus/Protocols/http/CorvusResponse.swift
@@ -0,0 +1,14 @@
+import Vapor
+
+/// `CorvusResponse` is a wrapper type for the result of`QueryEndpoint`s. Can
+/// be used to add metadata to a response.
+///
+public protocol CorvusResponse: Content {
+    
+    /// The item is equivalent to the `QueryEndpoint`'s `QuerySubject`.
+    associatedtype Item
+    
+    /// Initialises a `CorvusResponse` with a given item.
+    /// Normally this is the result of the `QueryEndpoints`'s handler function.
+    init(item: Item)
+}
